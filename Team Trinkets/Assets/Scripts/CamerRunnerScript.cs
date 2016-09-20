ca4be4a39863b2cbc408b0bc53fb4eb438a95412
LoganSilcock <logan.silcock@gmail.com>
2016-09-20 18:20:56 +1200

    Basic Game
    Real basic update using the sample assests. Need to finish destroy
    function and finalize mechanics.

---
+++ Team Trinkets/Assets/Scripts/CamerRunnerScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamerRunnerScript : MonoBehaviour {
+	public Transform player;
+
+	private float screenUp = 0;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (0, screenUp, -10);
+		screenUp = screenUp + 0.01f;
+	}
+}
---
+++ Team Trinkets/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public GameObject[] obj;
+	public float spawnMin = 10f;
+	public float spawnMax = 15f;
+
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+	
+	void Spawn(){
+		Instantiate (obj[Random.Range (0, obj.GetLength(0))], transform.position, Quaternion.identity);
+		Invoke ("Spawn", 20);//Random.Range (spawnMin, spawnMax);
+	}
+}
