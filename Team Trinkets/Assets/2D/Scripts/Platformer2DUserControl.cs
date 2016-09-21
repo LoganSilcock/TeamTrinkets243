bc56ced74f8bb282727e2eef8abd88ac4c22ebe3
LoganSilcock <logan.silcock@gmail.com>
2016-09-21 18:24:28 +1200

    Updated 2 Characters
    Heyo added another chacter into the scene (left controlled by A D, Space
    to jump, right controlled by Left and Right Arrows, Numpad 0 to jump).
    Jump animation not working in one of the characters yet to find out why.

---
+++ Team Trinkets/Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace UnitySampleAssets._2D
+{
+
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D character;
+        private bool jump;
+
+        private void Awake()
+        {
+            character = GetComponent<PlatformerCharacter2D>();
+        }
+
+        private void Update()
+        {
+            if(!jump)
+            // Read the jump input in Update so button presses aren't missed.
+				jump = Input.GetKeyDown(KeyCode.Keypad0); //CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+			float h = 0;
+			if (Input.GetKey (KeyCode.RightArrow))
+				h = 1;
+			if (Input.GetKey (KeyCode.LeftArrow))
+				h = -1;
+			//float h = Input.GetAxis("Horizontal"); //CrossPlatformInputManager.GetAxis("Horizontal");
+            // Pass all parameters to the character control script.
+            character.Move(h, jump);
+            jump = false;
+        }
+    }
+}
---
+++ Team Trinkets/Assets/2D/Scripts/Platformer2DUserControl2.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace UnitySampleAssets._2D
+{
+
+	[RequireComponent(typeof (PlatformerCharacter2D2))]
+	public class Platformer2DUserControl2 : MonoBehaviour
+	{
+		private PlatformerCharacter2D2 character;
+		private bool jump;
+
+		private void Awake()
+		{
+			character = GetComponent<PlatformerCharacter2D2>();
+		}
+
+		private void Update()
+		{
+			if(!jump)
+				// Read the jump input in Update so button presses aren't missed.
+				jump = Input.GetKeyDown(KeyCode.Space); //CrossPlatformInputManager.GetButtonDown("Jump");
+		}
+
+		private void FixedUpdate()
+		{
+			// Read the inputs.
+			float h = 0;
+			if (Input.GetKey (KeyCode.D))
+				h = 1;
+			if (Input.GetKey (KeyCode.A))
+				h = -1;
+			//float h = Input.GetAxis("Horizontal"); //CrossPlatformInputManager.GetAxis("Horizontal");
+			// Pass all parameters to the character control script.
+			character.Move(h, jump);
+			jump = false;
+		}
+	}
+}
